f22a3c322252d726067616b0d54a659dea1cf8eb
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2024-08-27 14:36:13 +0100

    singleSwapV3Pass

--- test/CrossChainArbitrage.t.sol
+++ test/CrossChainArbitrage.t.sol
@@ -15,16 +15,31 @@ contract TesCrossChainArbitrage is Test {
 
     address public owner;
 
+    /**
+     * @notice mainnet address
+     */
+    IERC20 public constant DAI =
+        IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    IERC20 public constant WETH =
+        IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 public constant USDC =
+        IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+
+    /**
+     * @notice arbitrum address
+     */
     address public constant UNISWAP_V2_ROUTER =
         0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
     address public constant UNISWAP_V3_ROUTER =
-        0x5E325eDA8064b456f4781070C0738d849c824258;
-    IERC20 public constant WETH =
-        IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
-    IERC20 public constant USDC =
-        IERC20(0xaf88d065e77c8cC2239327C5EDb3A432268e5831); // Arb USDC
-    IERC20 public constant DAI =
-        IERC20(0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1);
+        0xE592427A0AEce92De3Edee1F18E0157C05861564;
+
+    // 0x5E325eDA8064b456f4781070C0738d849c824258;
+    // IERC20 public constant WETH =
+    //     IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+    // IERC20 public constant USDC =
+    //     IERC20(0xaf88d065e77c8cC2239327C5EDb3A432268e5831); // Arb USDC
+    // IERC20 public constant DAI =
+    //     IERC20(0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1);
 
     function setUp() public {
         owner = msg.sender;
@@ -67,10 +82,10 @@ contract TesCrossChainArbitrage is Test {
 
     function testSwapOnUniswapV3() public {
         uint256 amountIn = 1e18;
-        uint256 amountOutMin = 0;
+        uint256 amountOutMin = 2000e6;
         uint24 fee = 3000;
 
-        arbitrageContract.multiSwapOnUniswapV3(
+        arbitrageContract.swapOnUniswapV3(
             address(WETH),
             address(USDC),
             amountIn,
@@ -79,12 +94,12 @@ contract TesCrossChainArbitrage is Test {
         );
 
         uint256 UsdcBalance = USDC.balanceOf(address(this));
-        console.log("USDC balance:", daiBalance);
-        // assertGt(
-        //     UsdcBalance,
-        //     amountOutMin,
-        //     "USDC balance should be greater than amountOutMin"
-        // );
+        console.log("USDC balance:", UsdcBalance);
+        assertGt(
+            UsdcBalance,
+            amountOutMin,
+            "USDC balance should be greater than amountOutMin"
+        );
     }
 
     function testMultihopSwapOnUniswapV3() public {
