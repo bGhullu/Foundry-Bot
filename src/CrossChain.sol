e2482c76d25c8c3a8d7b69484d84354837806eee
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2024-09-23 16:31:21 +0100

    level2

--- src/CrossChain.sol
+++ src/CrossChain.sol
@@ -507,25 +507,7 @@ contract CrossChain is Ownable, OApp, IFlashLoanReceiver {
         address[] memory assets,
         uint256[] memory amounts,
         uint256[] memory premiums
-    ) internal {
-        require(
-            assets.length == amounts.length,
-            "Mismatched assets and amounts"
-        );
-        require(
-            assets.length == premiums.length,
-            "Mismatched assets and premiums"
-        );
-
-        for (uint i = 0; i < assets.length; i++) {
-            // Repay each flash loan
-            uint amountOwed = amounts[i] + premiums[i];
-
-            IERC20(assets[i]).approve(address(lendingPool), amountOwed);
-            // Assuming the function to repay looks something like this:
-            // IERC20(assets[i]).transfer(address(lendingPool), amountOwed);
-        }
-    }
+    ) internal {}
 
     function _waitForBridgeCompletion(
         address token,
