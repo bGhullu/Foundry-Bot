523e9cd598169817e4e8282076e287c1f3fab37c
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2024-10-23 17:27:59 +0100

    TargetContract

--- src/TargetContract.sol
+++ src/TargetContract.sol
@@ -6,7 +6,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
 import {OApp, Origin, MessagingFee} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
 import "@aave/contracts/flashloan/interfaces/IFlashLoanSimpleReceiver.sol";
 import "@aave/contracts/interfaces/IPoolAddressesProvider.sol";
