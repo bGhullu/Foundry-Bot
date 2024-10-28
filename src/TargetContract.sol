549003a300f05943f6525f868e18575e3bee8db6
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2024-10-28 19:17:13 +0000

    TargetContract

--- src/TargetContract.sol
+++ src/TargetContract.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.20;
 
 
-import {OApp, Origin, MessagingFee} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
 import "@aave/contracts/flashloan/interfaces/IFlashLoanSimpleReceiver.sol";
 import "@aave/contracts/interfaces/IPoolAddressesProvider.sol";
 import "@aave/contracts/interfaces/IPool.sol";
