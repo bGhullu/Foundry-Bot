a8d18956975a304626793ffc20e3db649eaeb908
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2024-10-30 16:47:34 +0000

    TargetContract

--- src/TargetContract.sol
+++ src/TargetContract.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.20;
 
 import "@aave/contracts/interfaces/IPoolAddressesProvider.sol";
-import "@aave/contracts/interfaces/IPool.sol";
+
 
 contract TargetContract is OApp, Ownable {
     constructor(address mainContract) OApp(mainContract) Ownable(msg.sender) {}
